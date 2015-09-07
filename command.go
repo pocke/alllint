29d780584673bc9cc5135f200b560effebb8c707
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-09-07 21:15:41 +0900

    Execute command, but not parse yet.

--- command.go
+++ command.go
@@ -1,7 +1,14 @@
 package main
 
+import "os/exec"
+
 type Command []string
 
 func (c *Command) Exec() ([]Failure, error) {
+	_, err := exec.Command((*c)[0], (*c)[1:]...).Output()
+	if err != nil {
+		return nil, err
+	}
+	// TODO: parse
 	return nil, nil
 }
